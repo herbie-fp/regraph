7c3323bff0305d3bc195d58ff19a58952e4ec2fb
Pavel Panchekha <me@pavpanchekha.com>
2020-05-01 15:03:18 -0600

    More simplification to the precompute code

--- infra/precompute.rkt
+++ infra/precompute.rkt
@@ -1,28 +1,23 @@
 #lang racket
 ;; code extracted and simplified from Herbie v1.4 master April 25 2020
-(require math/flonum math/base math/special-functions math/bigfloat)
+(require math/flonum math/base math/special-functions math/bigfloat racket/hash)
 
 
 (provide eval-application)
 
+(define (cbrt x) (expt x (/ 1 3)))
+(define (exp2 x) (expt 2 x))
+(define (log10 x) (log x 10))
+(define (copysign x y) (if (>= y 0) (abs x) (- (abs x))))
+(define (fdim x y) (max (- x y) 0))
+(define (fma x y z) (bigfloat->flonum (bf+ (bf* (bf x) (bf y)) (bf z))))
+(define (fmax x y) (cond ((nan? x) y) ((nan? y) x) (else (max x y))))
+(define (fmin x y) (cond ((nan? x) y) ((nan? y) x) (else (min x y))))
+(define (logb x) (floor (log (abs x) 2)))
 
-(define ((comparator test) . args)
-  (for/and ([left args] [right (cdr args)])
-    (test left right)))
-
-(define (from-bigfloat bff)
-  (λ args (bigfloat->flonum (apply bff (map bf args)))))
-
-(define (bffmod x mod)
-  (bf- x (bf* (bftruncate (bf/ x mod)) mod)))
-
-(define (val-to-type val)
-  (cond
-    [(boolean? val)
-     (if val 'TRUE 'FALSE)]
-    [(real? val)
-     val]
-    [else (error "Unknown type" val)]))
+(define real-fns
+  (list + - * / acos acosh asin asinh atan atanh cbrt copysign cos cosh erf erfc exp exp2
+        fdim floor fma fmax fmin log log10 logb remainder round sin sinh sqrt tan tanh))
 
 (define (no-complex fun)
   (λ xs
@@ -31,6 +26,38 @@
        res
        +nan.0)))
 
+(define real-operations
+  (for/hash ([fn real-fns])
+    (values (object-name fn) (no-complex fn))))
+
+
+(define (from-bigfloat bff)
+  (λ args (bigfloat->flonum (apply bff (map bf args)))))
+
+(define (bffmod x mod)
+  (bf- x (bf* (bftruncate (bf/ x mod)) mod)))
+
+(define from-bf-operations
+  (hash 'expm1 (from-bigfloat bfexpm1)
+        'fmod (from-bigfloat bffmod)
+        'hypot (from-bigfloat bfhypot)
+        'j0 (from-bigfloat bfbesj0)
+        'j1 (from-bigfloat bfbesj1)
+        'log1p (from-bigfloat bflog1p)
+        'log2 (from-bigfloat bflog2)
+        'y0 (from-bigfloat bfbesy0)
+        'y1 (from-bigfloat bfbesy1)))
+
+
+(define complex-operations
+  (hash '+.c + '-.c - '*.c * '/.c / 'exp.c exp 'log.c log 'pow.c expt 'sqrt.c sqrt
+        'complex make-rectangular 're real-part 'im imag-part 'conj conjugate 'neg.c -))
+
+
+(define ((comparator test) . args)
+  (for/and ([left args] [right (cdr args)])
+    (test left right)))
+
 (define (if-fn test if-true if-false) (if test if-true if-false))
 (define (and-fn . as) (andmap identity as))
 (define (or-fn  . as) (ormap identity as))
@@ -38,96 +65,31 @@
 (define (!=-fn . args)
   (not (check-duplicates args =)))
 
-(define (cbrt x)
-  (expt x (/ 1 3)))
-(define (exp2 x)
-  (expt 2 x))
-(define (log10 x)
-  (log x 10))
-(define (copysign x y)
-  (if (>= y 0) (abs x) (- (abs x))))
-(define (fdim x y)
-  (max (- x y) 0))
-(define (fma x y z)
-  (bigfloat->flonum (bf+ (bf* (bf x) (bf y)) (bf z))))
+(define other-operations
+  (hash 'ceil ceiling
+        'atan2 (no-complex atan)
+        'fabs abs
+        'lgamma log-gamma
+        'pow (no-complex expt)
+        'rint round
+        'tgamma gamma
+        'trunc truncate
+        '== (comparator =)
+        '!= !=-fn
+        '< (comparator <)
+        '> (comparator >)
+        '<= (comparator <=)
+        '>= (comparator >=)
+        'if if-fn
+        'not not
+        'and and-fn
+        'or or-fn))
 
+(define operations
+  (hash-union complex-operations real-operations from-bf-operations other-operations))
 
-(define operator-pairs
-  `((+ ,+)
-    (+.c ,+)
-    (- ,-)
-    (neg.c ,-)
-    (-.c ,-)
-    (* ,*)
-    (*.c ,*)
-    (/ ,/)
-    (/.c ,/)
-    (acos ,(no-complex acos))
-    (acosh ,(no-complex acosh))
-    (asin ,(no-complex asin))
-    (asinh ,(no-complex asinh))
-    (atan ,(no-complex atan))
-    (atan2 ,(no-complex atan))
-    (atanh ,(no-complex atanh))
-    (cbrt ,(no-complex cbrt))
-    (ceil ,ceiling)
-    (copysign ,copysign)
-    (cos ,cos)
-    (cosh ,cosh)
-    (erf ,(no-complex erf))
-    (erfc ,erfc)
-    (exp ,exp)
-    (exp.c ,exp)
-    (exp2 ,(no-complex exp2))
-    (expm1 ,(from-bigfloat bfexpm1))
-    (fabs ,abs)
-    (fdim ,fdim)
-    (floor ,(lambda (x) (floor x)))
-    (fma ,fma)
-    (fmax ,(lambda (x y) (cond ((nan? x) y) ((nan? y) x) (else (max x y)))))
-    (fmin ,(lambda (x y) (cond ((nan? x) y) ((nan? y) x) (else (min x y)))))
-    (fmod ,(from-bigfloat bffmod))
-    (hypot ,(from-bigfloat bfhypot))
-    (j0 ,(from-bigfloat bfbesj0))
-    (j1 ,(from-bigfloat bfbesj1))
-    (lgamma ,log-gamma)
-    (log ,(no-complex log))
-    (log.c ,log)
-    (log10 ,(no-complex log10))
-    (log1p ,(from-bigfloat bflog1p))
-    (log2 ,(from-bigfloat bflog2))
-    (logb ,(lambda (x) (floor (bigfloat->flonum (bflog2 (bf (abs x)))))))
-    (pow ,(no-complex expt))
-    (pow.c ,expt)
-    (remainder ,remainder)
-    (rint ,round)
-    (round ,round)
-    (sin ,sin)
-    (sinh ,sinh)
-    (sqrt ,(no-complex sqrt))
-    (sqrt.c ,sqrt)
-    (tan ,tan)
-    (tanh ,tanh)
-    (tgamma ,gamma)
-    (trunc ,truncate)
-    (y0 ,(from-bigfloat bfbesy0))
-    (y1 ,(from-bigfloat bfbesy1))
-    (if ,if-fn)
-    (== ,(comparator =))
-    (complex ,make-rectangular)
-    (re ,real-part)
-    (im ,imag-part)
-    (conj ,conjugate)
-    (!= ,!=-fn)
-    (< ,(comparator <))
-    (> ,(comparator >))
-    (<= ,(comparator <=))
-    (>= ,(comparator >=))
-    (not ,not)
-    (and ,and-fn)
-    (or ,or-fn)))
-
-(define op-hash (make-immutable-hash operator-pairs))
+(define (val-to-type val)
+  (match val [#t 'TRUE] [#f 'FALSE] [(? real?) val]))
 
 (define (exact-noncomplex-value? val)
   (and (not (and (complex? val) (not (real? val))))
@@ -136,9 +98,7 @@
 (define (eval-application op . args)
   (if (and (not (null? args)) (andmap (conjoin number? exact?) args))
       (with-handlers ([exn:fail:contract:divide-by-zero? (const #f)])
-        (define fn (first (hash-ref op-hash op)))
-        (define res (apply fn args))
-        
+        (define res (apply (hash-ref operations op) args))
         (and (exact-noncomplex-value? res)
              (val-to-type res)))
       false))
